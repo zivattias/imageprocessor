d0ca54f0e341a25566396ea8f883786468b6c327
zivattias <ziv.attias7@gmail.com>
2023-01-23 13:28:28 +0200

    sample pic & image_load.py

---
+++ src/load_image.py
@@ -0,0 +1,11 @@
+# load and display an image with Matplotlib
+from matplotlib import image
+from matplotlib import pyplot
+# load image as pixel array
+image = image.imread('kolala.jpeg')
+# summarize shape of the pixel array
+print(image.dtype)
+print(image.shape)
+# display the array of pixels as an image
+pyplot.imshow(image)
+pyplot.show()
